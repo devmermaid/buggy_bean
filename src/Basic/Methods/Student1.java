f2dfbf4eab558d76263ffa3dd7a3928662d65b55
dell <mohinibusiness98@gmail.com>
2026-05-03 18:41:06 +0530

    Jay Sunday

---
+++ src/Basic/Methods/Student1.java
@@ -0,0 +1,14 @@
+package Basic.Methods;
+
+public class Student1
+{
+    //using static field and static method
+    static int age=20;
+    public static int display()
+    {
+        return age;
+    }
+    static void main() {
+        System.out.println(Student1.display());
+    }
+}
