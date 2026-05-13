7457e3138ad9c146591312aecbacc90a5e31473a
dell <mohinibusiness98@gmail.com>
2026-05-13 18:15:07 +0530

    It was simple like my complex life!

---
+++ src/GeeksForGeeks/MultiTable.java
@@ -0,0 +1,14 @@
+package GeeksForGeeks;
+
+public class MultiTable
+{
+    static void main() {
+        int n=5;
+        int mul=1;
+        for(int i=1;i<=10;i++)
+        {
+            mul=n*i;
+            System.out.println(mul);
+        }
+    }
+}
