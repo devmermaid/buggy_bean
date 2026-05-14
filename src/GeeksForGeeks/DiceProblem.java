0f473d1273d1bd3a8a4bb8f2f5cfd8153b3c4b64
dell <mohinibusiness98@gmail.com>
2026-05-14 16:05:28 +0530

    Dice and dice

--- src/GeeksForGeeks/DiceProblem.java
+++ src/GeeksForGeeks/DiceProblem.java
@@ -2,6 +2,15 @@ package GeeksForGeeks;
 
 import java.util.Scanner;
 
+/* You are given a cubic dice with 6 faces.
+All the individual faces have a number printed on them.
+The numbers are in the range of 1 to 6, like any ordinary dice.
+You will be provided with a face of this cube,
+your task is to guess the number on the opposite face of the cube.
+ */
+
+//approach using switch
+//we can simple if else as well, simplest approach
 public class DiceProblem
 {
     public static int show(int n)
