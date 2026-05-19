924a94d92b4accbe03af5241aeb5b9f75d5063bf
dell <mohinibusiness98@gmail.com>
2026-05-19 11:57:37 +0530

    Chup chap line me lago :) hehehe

--- src/CollectionFramework/Queen.java
+++ src/CollectionFramework/Queen.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Queen
 {
     static void main() {
-        Queue<String> queue=new LinkedList<>();//here we can implement queue using array or LL
+        Queue<String> queue=new LinkedList<>();//here we can implement queue using arrayDeque or LL
         queue.add("Mohini");
         queue.offer("Rudraksh");
         System.out.println(queue);
