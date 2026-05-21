3ad748cd2bede0e4edcfb61f0318fa599344c341
dell <mohinibusiness98@gmail.com>
2026-05-21 16:13:33 +0530

    OH MY GOD, I WROTE THIS LONG PROGRAM< SHIT, I CAN DO IT :) YAYYYYYYY

--- src/CollectionFramework/ArrayList1.java
+++ src/CollectionFramework/ArrayList1.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class ArrayList1
 {
+    //constructor to send data directly from object to arraylist
     String name;
     double price;
     ArrayList1(String name, double price)
@@ -13,37 +14,39 @@ public class ArrayList1
     }
 
     static void main() {
-        Scanner sc=new Scanner(System.in);
-        ArrayList<ArrayList1> cart=new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        //arraylist for cart, as adding items in cart
+        ArrayList<ArrayList1> cart = new ArrayList<>();
+
+        //number of items of cart
         System.out.println("Enter number of items for cart: ");
-        int totalItems=sc.nextInt();
-        for(int i=1;i<=totalItems;i++)
-        {
+        int totalItems = sc.nextInt();
+
+        // adding items in cart
+        for (int i = 1; i <= totalItems; i++) {
             System.out.println("Enter item name: ");
-            String name=sc.next();
+            String name = sc.next();
             System.out.println("Enter item price");
-            double price=sc.nextDouble();
-            cart.add(new ArrayList1(name, price));
+            double price = sc.nextDouble();
+            cart.add(new ArrayList1(name, price)); // here directly after accepting value, we will add it to arraylist
         }
-        //print cart
-        double totalBill=0;
-        for(ArrayList1 item:cart)
-        {
-            System.out.println("Name: "+ item.name+" Price: "+item.price);
-            totalBill+=item.price;
+
+        //print cart along with totaling final
+        double totalBill = 0;
+        for (ArrayList1 item : cart) {
+            System.out.println("Name: " + item.name + " Price: " + item.price);
+            totalBill += item.price;
         }
-        System.out.println("Total Bill: "+ totalBill);
+
         //add discount
-        if(totalBill>=100)
-        {
-            double discount=10.0;
-            totalBill-=discount;
+        if (totalBill >= 100) {
+            double discount = 10.0;
+            totalBill -= discount;
             System.out.println("Discount applied: " + discount);
-        }
-        else
-        {
+        } else {
             System.out.println("Shop for more than 100 to receive the discount");
         }
+        //printing final bill after discount
         System.out.println("Final bill: " + totalBill);
     }
 }
