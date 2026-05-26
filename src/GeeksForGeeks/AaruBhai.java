78931ef5a9cf0591e07f30537a9bd1f4f1eeedd1
dell <mohinibusiness98@gmail.com>
2026-05-26 08:01:17 +0530

    I don't know how to be men, so no main in this :)

---
+++ src/GeeksForGeeks/AaruBhai.java
@@ -0,0 +1,15 @@
+package GeeksForGeeks;
+
+public class AaruBhai {
+    //You are given an array arr[] that contains integers.
+    // You need to print the elements of the array in a single line with a space between them.
+   // Note: Don't add a new line at the end.
+
+    public static void arrayTraversal(int[] arr) {
+        // Code here
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(" "+arr[i]);
+        }
+    }
+}
