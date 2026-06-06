f51cf0031a0930399bfd4935c0d3969f0e8d1775
dell <mohinibusiness98@gmail.com>
2026-06-06 21:13:35 +0530

    My life has more drama than gopi bahu

--- src/GeeksForGeeks/ThisisPostitive.java
+++ src/GeeksForGeeks/ThisisPostitive.java
@@ -2,7 +2,8 @@ package GeeksForGeeks;
 
 public class ThisisPostitive
 {
-    //You are given a String S, you need to print its characters at even indices(index starts at 0).
+    //You are given a String S, you need to print its characters
+    // at even indices(index starts at 0).
     static void main() {
         String s="Mohini";
         for(int i=0;i<s.length();i++)
