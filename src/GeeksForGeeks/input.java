9ffef417891db68756ce9ed237487df70e152c63
dell <mohinibusiness98@gmail.com>
2026-06-21 20:43:06 +0530

    unknown

---
+++ src/GeeksForGeeks/input.java
@@ -0,0 +1,16 @@
+package GeeksForGeeks;
+
+import java.util.Scanner;
+
+public class input
+{
+    static void main() {
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value for a and b: ");
+        String a = sc.next();
+        String b = sc.next();
+        System.out.print(a+" "+b);
+        System.out.println();
+    }
+}
