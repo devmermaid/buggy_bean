bd4d52b1f88744fc29e9a0f28c1f3b5ccd41cd34
dell <mohinibusiness98@gmail.com>
2026-06-22 20:23:03 +0530

    Slow day

---
+++ src/GeeksForGeeks/func3.java
@@ -0,0 +1,20 @@
+package GeeksForGeeks;
+//Given a string S and an integer number N, print the output in the form: "The input string :S" and "The input integer :N" in the two separate lines.
+
+//Example 1:
+
+//Input:
+//S = 'GeeksforGeeks'
+//N = 20
+//Output :
+//The input string :GeeksforGeeks
+//The input integer :20
+public class func3
+{
+    static void main() {
+        String S = "GeeksforGeeks";
+        int N=20;
+        System.out.println("The input string :"+S);
+        System.out.println("The input integer :"+N);
+    }
+}
