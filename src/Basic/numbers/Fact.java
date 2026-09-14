065a15bb889b06ff9c22078cb7692aa50c5c221d
dell <mohinibusiness98@gmail.com>
2026-09-14 19:29:52 +0530

    I am healing again!!! Let's rock it again. We won't give up.

--- src/Basic/numbers/Fact.java
+++ src/Basic/numbers/Fact.java
@@ -3,7 +3,15 @@ package Basic.numbers;
 public class Fact
 {
     static void main() {
-        int num=3;
+        /*int num=3;
+        int fact=1;
+        for(int i=1;i<=num;i++)
+        {
+            fact=fact*i;
+        }
+        System.out.println(fact);*/
+
+        int num=4;
         int fact=1;
         for(int i=1;i<=num;i++)
         {
